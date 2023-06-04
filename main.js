570e2e6e29f372f5807e7873f83dbac00cedf032
harrisfoes <11287035+harrisfoes@users.noreply.github.com>
2023-06-04 21:27:33 +0800

    first pass

---
+++ main.js
@@ -0,0 +1,9 @@
+/*
+add
+subtract 
+multiply
+divide
+You should round answers with long decimals so that they don’t overflow the screen.
+Pressing = before entering all of the numbers or an operator could cause problems!
+Display a snarky error message if the user tries to divide by 0… and don’t let it crash your calculator!
+*/
