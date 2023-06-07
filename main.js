84b5c5775732a96de256b65f341699b428e9d03c
harrisfoes <11287035+harrisfoes@users.noreply.github.com>
2023-06-07 19:50:28 +0800

    last pass with polish

--- main.js
+++ main.js
@@ -16,110 +16,148 @@ const equal = document.querySelector('.equal');
 const del = document.querySelector('.delete');
 const decimal = document.querySelector('.decimal');
 
-numberBtn.forEach(function(e) {
-    e.addEventListener('click', function(){
-
-        //clean history when number pressed after equal
-        if(currentNum == '' && storedOperator == ''){
-            secScreen.textContent = '';
-            storedNum = '';
-        }
-        //disallow zero when empty
-        if(currentNum == '' && e.textContent == '0')
-            return;
-
-        appendNumber(e.textContent);
-        mainScreen.textContent = currentNum;
-    });
+numberBtn.forEach(function (e) {
+    e.addEventListener('click', () => receiveNumber(e.textContent));
 });
 
-operatorBtn.forEach(function(e) {
-    e.addEventListener('click', function(){
-        if(storedOperator == ''){
-            storedOperator = e.textContent;
-            if(storedNum == '')
-                storedNum = currentNum;
-            currentNum = '';
-            secScreen.textContent = handleResultLength(storedNum) + " " + storedOperator;
-        }
-        if(currentNum != '' && storedNum != ''){
-            storedNum = operate().toString();
-            mainScreen.textContent = storedNum;
-            storedOperator = e.textContent;
-            secScreen.textContent =  handleResultLength(storedNum) + " " + storedOperator;
-            currentNum = '';
-        }
-    });
+operatorBtn.forEach(function (e) {
+    e.addEventListener('click', () => handleOperator(e.textContent));
 });
 
-del.addEventListener('click', function(){
-    currentNum = currentNum.slice(0,-1);
+del.addEventListener('click', () => handleDelete());
 
-    //current num empty and stored num has value after calculating, refuse del then
-    if(storedOperator != '' && currentNum == '')
-        mainScreen.textContent = '0';   
-    else if(storedNum != '' && currentNum == '')
+equal.addEventListener('click', () => handleEqual());
+
+clear.addEventListener('click', () => handleClear());
+
+decimal.addEventListener('click', () => handleDecimal());
+
+window.addEventListener('keydown', handleKeypress);
+
+function appendNumber(num) {
+    if (currentNum == '0' && num == '0')
         return;
-    if(currentNum == '')
-        mainScreen.textContent = '0';
-    else{
-        mainScreen.textContent = currentNum;
-    }
-});
+    else if (currentNum == '0' && num > 0)
+        currentNum = num;    
+    else if (currentNum.length < 9)
+        currentNum = currentNum + num;   
+ 
+}
 
-equal.addEventListener('click', function(){
-    if(currentNum != '' && storedNum != '' && storedOperator != ''){
+function operate() {
+    if (storedOperator == '+') {
+        return Number(storedNum) + Number(currentNum);
+    } else if (storedOperator == '-') {
+        return Number(storedNum) - Number(currentNum);
+    } else if (storedOperator == '×') {
+        return Number(storedNum) * Number(currentNum);
+    } else if (storedOperator == '÷' || storedOperator == '/') {
+        if(Number(currentNum) == 0)
+            return 'Bruh..';
+        return Number(storedNum) / Number(currentNum);
+    }
+}
+
+function handleResultLength(result) {
+    result = result.toString();
+
+    if (result.includes(".") && result.length > digitLimit) {
+        return result.substr(0, digitLimit);
+    } else if (result.length > digitLimit) {
+        return result.substr(0, digitLimit) + "...";
+    }
+
+    return result;
+
+}
+
+function receiveNumber(num) {
+
+    //clean history when number pressed after equal
+    if (currentNum == '' && storedOperator == '') {
+        secScreen.textContent = '';
+        storedNum = '';
+    }
+    appendNumber(num);
+    mainScreen.textContent = currentNum;
+}
+
+function handleOperator(op) {
+    if (storedOperator == '') {
+        storedOperator = op;
+        if (storedNum == '')
+            storedNum = currentNum;
+        currentNum = '';
+        secScreen.textContent = handleResultLength(storedNum) + " " + storedOperator;
+    }
+    if (currentNum != '' && storedNum != '') {
+        storedNum = operate().toString();
+        mainScreen.textContent = storedNum;
+        storedOperator = op;
+        secScreen.textContent = handleResultLength(storedNum) + " " + storedOperator;
+        currentNum = '';
+    }
+}
+
+function handleEqual() {
+    if (currentNum != '' && storedNum != '' && storedOperator != '') {
         secScreen.textContent = '';
         storedNum = operate().toString();
         mainScreen.textContent = handleResultLength(storedNum);
         storedOperator = '';
         currentNum = '';
     }
-});
-    
-clear.addEventListener('click', function(){
+}
+
+function handleClear() {
     currentNum = '';
     storedNum = '';
     storedOperator = '';
     mainScreen.textContent = '0';
     secScreen.textContent = '';
-});
+}
 
-decimal.addEventListener('click', function(){
-    if(!currentNum.includes(".")){
+function handleDecimal() {
+    if (currentNum == '' || currentNum == '0'){
+        currentNum = "0.";
+        mainScreen.textContent = currentNum;
+    }
+    if (!currentNum.includes(".")) {
         currentNum += ".";
         mainScreen.textContent = currentNum;
     }
-});
-
-function appendNumber(num){
-    if(currentNum.length < 9)
-        currentNum = currentNum + num;
 }
 
-function operate(){
-    if (storedOperator == '+'){
-        return Number(storedNum) + Number(currentNum);
-    }else if(storedOperator == '-'){
-        return Number(storedNum) - Number(currentNum);
-    }else if(storedOperator == '×'){
-        return Number(storedNum) * Number(currentNum);
-    }else if(storedOperator == '÷'){
-        return Number(storedNum) / Number(currentNum);
+function handleDelete() {
+    currentNum = currentNum.slice(0, -1);
+
+    //current num empty and stored num has value after calculating, refuse del then
+    if (storedOperator != '' && currentNum == '')
+        mainScreen.textContent = '0';
+    else if (storedNum != '' && currentNum == '')
+        return;
+    if (currentNum == '')
+        mainScreen.textContent = '0';
+    else {
+        mainScreen.textContent = currentNum;
     }
 }
 
-function handleResultLength(result){
-    result = result.toString();
+function handleKeypress(e) {
+    e.preventDefault();
 
-    if(result.includes(".") && result.length > digitLimit){
-        const toRemove = result.length - digitLimit;
-        //result = Number(result);
-        return result.substr(0,digitLimit);
-    }else if(result.length > digitLimit){
-        return result.substr(0,digitLimit) + "...";
-    }
-
-    return result;
-
+    if (e.key >= 0 && e.key <= 9)
+        receiveNumber(e.key);
+    else if (e.key == 'Enter' || e.key == '=')
+        handleEqual();
+    else if (e.key == '+' || e.key == "/" || e.key == '-')
+        handleOperator(e.key)
+    else if (e.key == '*')
+        handleOperator("×")
+    else if (e.key == '.')
+        handleDecimal()
+    else if (e.key == 'Backspace')
+        handleDelete();
+    else if (e.key == 'Escape')
+        handleClear();
 }
